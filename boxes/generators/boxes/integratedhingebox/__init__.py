c5c56e56bbaa4f471fd9e8ac4ba99b0bd76a2a35
Axel CATOIRE <axel.catoire@gmail.com>
2026-05-05 21:33:21 +0200

    refactor: optimize rectangularWall calls for better readability and structure

--- boxes/generators/boxes/integratedhingebox/__init__.py
+++ boxes/generators/boxes/integratedhingebox/__init__.py
@@ -27,12 +27,10 @@ class IntegratedHingeBox(Boxes):
         self.addSettingsArgs(edges.ChestHingeSettings)
         self.buildArgParser("x", "y", "h", "outside")
         self.argparser.add_argument(
-            "--lidheight",  action="store", type=float, default=20.0,
+            "--lidheight", action="store", type=float, default=20.0,
             help="height of lid in mm")
 
-
     def render(self):
-
         x, y, h, hl = self.x, self.y, self.h, self.lidheight
 
         if self.outside:
@@ -45,18 +43,22 @@ class IntegratedHingeBox(Boxes):
         hy = self.edges["O"].startWidth()
         hy2 = self.edges["P"].startWidth()
 
-        e1 = edges.CompoundEdge(self, "Fe", (h-hy, hy))
-        e2 = edges.CompoundEdge(self, "eF", (hy, h-hy))
+        e1 = edges.CompoundEdge(self, "Fe", (h - hy, hy))
+        e2 = edges.CompoundEdge(self, "eF", (hy, h - hy))
         e_back = ("F", e1, "e", e2)
 
-        self.rectangularWall(y, h-hy, "FfOf", ignore_widths=[2], move="up")
-        self.rectangularWall(y, hl-hy2, "pfFf", ignore_widths=[1], move="up")
-        self.rectangularWall(y, h-hy, "Ffof", ignore_widths=[5], move="up")
-        self.rectangularWall(y, hl-hy2, "PfFf", ignore_widths=[6], move="up")
-        self.rectangularWall(x, h, "FFeF", move="up")
-        self.rectangularWall(x, h, e_back, move="up")
-        self.rectangularWall(x, hl, "FFeF", move="up")
-        self.rectangularWall(x, hl-hy2, "FFqF", move="up")
-
-        self.rectangularWall(y, x, "ffff", move="up")
-        self.rectangularWall(y, x, "ffff")
+        # Row 1 ─ col1: move="right"  col2: move="up"
+        self.rectangularWall(y, h - hy, "FfOf", ignore_widths=[2], move="right", label="Side Right")
+        self.rectangularWall(y, h - hy, "Ffof", ignore_widths=[5], move="up", label="Side Left")
+        # Row 2 ─ col2: no move (cursor already at col2)  col1: move="left up" (jump left → col1, then up)
+        self.rectangularWall(y, hl - hy2, "PfFf", ignore_widths=[6], label="Lid Side Left")
+        self.rectangularWall(y, hl - hy2, "pfFf", ignore_widths=[1], move="left up", label="Lid Side Right")
+        # Row 3
+        self.rectangularWall(x, h, "FFeF", move="right", label="Front")
+        self.rectangularWall(x, h, e_back, move="up", label="Back")
+        # Row 4
+        self.rectangularWall(x, hl - hy2, "FFqF", label="Lid Back")
+        self.rectangularWall(x, hl, "FFeF", move="left up", label="Lid Front")
+        # Row 5
+        self.rectangularWall(y, x, "ffff", move="right", label="Bottom")
+        self.rectangularWall(y, x, "ffff", move="up", label="Top")
